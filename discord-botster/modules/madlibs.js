6f5deee3599819301289c0491ff37496d0700b59
Geoff <geoffboyd2@gmail.com>
2022-09-07 00:12:44 -0400

    notes for clarity on madlibs

--- discord-botster/modules/madlibs.js
+++ discord-botster/modules/madlibs.js
@@ -7,10 +7,11 @@ module.exports = {
     const db = new SQLite('../db/userinputs.sqlite');
     const wordTypes = [['noun', "noun='true' OR noun_phrase='true'"],['plural', "plural='true'"],['verb', "verb_usu='true' OR verb_trans='true' OR verb_intrans='true'"],['adjective', "adjective='true'"],['adverb', "adjective='true'"],['conjunction', "conjunction='true'"],['preposition', "preposition='true'"],['interjection', "interjection='true'"],['pronoun', "pronoun='true'"],['article', "definite_article='true' OR indefinite_article='true'"],['nominative', "nominative='true'"]];
 
-    // Check if the table dictionary exists and has content.
+    // Check if the table 'dictionary' exists and has content.
     const table = db.prepare(`SELECT count(*) FROM dictionary`).get();
     if (!table['count(*)']) { return msg.channel.send("Uh oh, my dictionary is empty :("); }
 
+    // Loop through all the different wordTypes and turn the MadLip into a story
     for (let i=0; i<wordTypes.length; i++){
       let searchKey = `{${wordTypes[i][0]}}`
       while (madlibPhrase.includes(searchKey)) {
